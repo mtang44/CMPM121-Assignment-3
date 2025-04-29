0af7fded4172d28763aeaa6bbc29099c2f7c5871
mcst2014 <mcst2014@outlook.com>
2025-04-29 17:01:26 -0500

    Assignment 2 Start

--- Assets/Scripts/Spells/Spell.cs
+++ Assets/Scripts/Spells/Spell.cs
@@ -10,13 +10,14 @@ public class Spell
     public SpellCaster owner;
     public Hittable.Team team;
 
-    private string name;
-    private string description;
-    private int icon;
-    private string mana_cost;
-    private Dictionary<string, int> damage;
-    private int cooldown;
-    private Dictionary<string, string> projectile;
+    public string name;
+    public string description;
+    public int icon;
+    public string mana_cost;
+    public string damage;
+    public Damage.Type damage_type;
+    public string cooldown;
+    public Dictionary<string, string> projectile;
 
     public Spell(SpellCaster owner)
     {
@@ -25,7 +26,7 @@ public class Spell
 
     public string GetName()
     {
-        return "name";
+        return this.name;
         // return this.name;
     }
 
@@ -56,9 +57,29 @@ public class Spell
     {
         return (last_cast + GetCooldown() < Time.time);
     }
-
-    public virtual IEnumerator Cast(Vector3 where, Vector3 target, Hittable.Team team)
+     public virtual void SetAttributes(JObject attributes)
     {
+         "name": "Arcane Bolt",
+       "description": "A straight-flying bolt.",
+    
+       "damage": {"amount": "25 power 5 / +", "type": "arcane"},
+       "mana_cost": "10",
+       "cooldown": "2",
+       "projectile":
+        name = attributes["name"].ToString();
+        description = attributes["description"].ToString();
+        icon = attributes["icon"].ToObject<int>();
+        damage = attributes["damage"]["amount"].ToString();
+        damage_type = Damage.TypeFromString(attributes["damage"]["type"].ToString());
+        cooldown = attributes["cooldown"].ToString();
+        projectile = attributes["projectile"].ToObject<Projectile>();
+      
+      //...
+}
+    }
+    public virtual IEnumerator Cast(Vector3 where, Vector3 target, Hittable.Team team, ValueModifier? mods = null)
+    {
+        ValueModifier modifiers = mods ?? null;
         this.team = team;
         GameManager.Instance.projectileManager.CreateProjectile(0, "straight", where, target - where, 15f, OnHit);
         yield return new WaitForEndOfFrame();
--- Assets/Scripts/Spells/SpellBuilder.cs
+++ Assets/Scripts/Spells/SpellBuilder.cs
@@ -3,33 +3,70 @@ using System.IO;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using UnityEngine.Rendering.Universal;
 
 
 public class SpellBuilder 
 {
-    private Dictionary <string, Spell> spell_list;
-    public Spell Build(SpellCaster owner)
+    private Dictionary <string, JObject> spell_attributes;
+    private List <string> spell_names;
+
+
+    public Spell BuildSpell(string name, SpellCaster owner)
     {
-        return new Spell(owner);
+        Spell spell = MakeSpell(name);
+        spell.SetAttributes(spell_attributes[name]);
+        return spell;
     }
 
    
     public SpellBuilder()
     {        
+        readSpellsJson();
     }
-    public Dictionary<string, Spell> readSpellsJson()
+
+    private Spell MakeSpell(string name) {
+        if (name == "magic_missile") {
+            return new MagicMissile();
+        }
+        if(name == "arcane_blast"){
+            return new arcaneBlact();
+        }
+        if(name == "arcane_spray"){
+            return new ArcaneSpray();
+        }
+        if(name == "arcane_bolt"){
+            return new arcaneBolt();
+        }
+        if(name == "damage-amplified"){
+            return new damageAmplifier();
+        }
+        if(name == "speed-amplified"){
+            return new speedAmplified();
+        }
+        if(name == "doubled"){
+            return new doubled();
+        }
+        if(name == "split"){
+            return new split();
+        }
+        if(name == "chaotic"){
+            return new chaotic();
+        }
+        if(name == "homing"){
+            return new homing();
+        }
+    }
+
+    public void readSpellsJson()
      {
-        Dictionary<string, Spell> spell_types = new Dictionary<string, Spell>();
         var spelltext = Resources.Load<TextAsset>("spells");
         
-        JToken jo = JToken.Parse(spelltext.text);
-        foreach (var spell in jo)
+        spell_attributes = JObject.Parse(spelltext.text);
+        foreach(var a in spell_attributes)
         {
-           
-            Spell s = spell.ToObject<Spell>();// request construction of object NEED Enemy class first
-            spell_types[s.GetName()] = s;
+            spell_names.Add(a.Key);
         }
-        return spell_types;
-     }
-
+    }
 }
+
