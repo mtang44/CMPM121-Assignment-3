57d1d5086f99522135d028900467d4b0fde21158
mcst2014 <mcst2014@outlook.com>
2025-05-04 16:33:26 -0500

    worked on reward screen UI

--- Assets/Scripts/UI/RewardScreenManager.cs
+++ Assets/Scripts/UI/RewardScreenManager.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 public class RewardScreenManager : MonoBehaviour
 {
     public GameObject rewardUI;
-   
+    public TMP_Text SpellDescription;
+    public TMP_Text SpellName;
+    public GameObject icon;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       // enemiesKilledLabel = GetComponent<TextMeshProUGUI>();
+       
     }
 
     // Update is called once per frame
@@ -19,7 +21,12 @@ public class RewardScreenManager : MonoBehaviour
         {   
             rewardUI.SetActive(true);
            // enemiesKilledLabel.text = "Enemies Killed: " + GameManager.Instance.numEnemiesKilled;
-            //    Spell newRewardSpell = MakeRandomSpell();
+            //   ;
+            Spell newRewardSpell =  new SpellBuilder().MakeRandomSpell(GameManager.Instance.player.GetComponent<SpellCaster>());   
+            SpellDescription.text = "Spell Description " + newRewardSpell.description;
+            SpellName.text = "Spell Name " + newRewardSpell.name;
+            GameManager.Instance.spellIconManager.PlaceSprite(newRewardSpell.GetIcon(), icon.GetComponent<Image>());
+
         }
         else
         {
