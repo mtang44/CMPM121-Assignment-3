922bdcaa675dd3399c68479eb5193bd924ffd496
mcst2014 <mcst2014@outlook.com>
2025-05-06 01:56:14 -0500

    worked on UI display

--- Assets/Scripts/UI/RewardScreenManager.cs
+++ Assets/Scripts/UI/RewardScreenManager.cs
@@ -8,6 +8,7 @@ public class RewardScreenManager : MonoBehaviour
     public TMP_Text SpellDescription;
     public TMP_Text SpellName;
     public GameObject icon;
+    public Spell spell;
     public Spell newRewardSpell;
     public GameObject spell_1_drop;
     public GameObject spell_2_drop;
@@ -26,13 +27,21 @@ public class RewardScreenManager : MonoBehaviour
         {   
             rewardUI.SetActive(true);
            // enemiesKilledLabel.text = "Enemies Killed: " + GameManager.Instance.numEnemiesKilled;
-            //   ;
+            foreach(var s in GameManager.Instance.player.GetComponent<PlayerController>().activeSpells)
+            {
+                GameManager.Instance.player.GetComponent<SpellUI>().SetSpell(s);
+            }
             newRewardSpell =  new SpellBuilder().MakeRandomSpell(GameManager.Instance.player.GetComponent<SpellCaster>());   
             SpellDescription.text = "Spell Description " + newRewardSpell.description;
             SpellName.text = "Spell Name " + newRewardSpell.name;
             GameManager.Instance.spellIconManager.PlaceSprite(newRewardSpell.GetIcon(), icon.GetComponent<Image>());
-            
-
+            // deactivates spell drop button if spell is 
+            if(GameManager.Instance.player.GetComponent<PlayerController>().activeSpells.Count < 4){
+            spell_1_drop.SetActive(false);
+            spell_2_drop.SetActive(false);
+            spell_3_drop.SetActive(false);
+            spell_4_drop.SetActive(false);
+            }
         }
         else
         {
@@ -45,6 +54,7 @@ public class RewardScreenManager : MonoBehaviour
             GameManager.Instance.player.GetComponent<PlayerController>().activeSpells.Add(newRewardSpell);
         }
         else{
+
             spell_1_drop.SetActive(true);
             spell_2_drop.SetActive(true);
             spell_3_drop.SetActive(true);
@@ -52,11 +62,12 @@ public class RewardScreenManager : MonoBehaviour
         }
     } 
 
-    public void dropSpell()
+    public void dropSpell(int i)
     {
-       
+       GameManager.Instance.player.GetComponent<PlayerController>().activeSpells.RemoveAt(i);
         //GameManager.Instance.player.GetComponent<PlayerController>().activeSpells.Remove(removed);
             // figure out drop specific spell. 
     }
+    
 }
 
